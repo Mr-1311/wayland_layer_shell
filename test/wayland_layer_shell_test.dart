70b4a615790578e30d4d9c0b67cdbd2227eff2d2
Metin Ur <metinur.1311@gmail.com>
2023-12-11 00:25:50 +0300

    Remove tests

--- test/wayland_layer_shell_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:wayland_layer_shell/wayland_layer_shell.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockWaylandLayerShellPlatform {}
-
-void main() {}
